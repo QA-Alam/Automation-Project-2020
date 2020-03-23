9ae30f2cebeec1ca2f91548d662a4a8af9f61042
mohammedalam <mohammedalam@10.211.55.4>
2020-03-22 22:49:50 -0400

    Alam

--- Automation Project 2020/src/test/java/com/usa/myrunner/MyRunner.java
+++ Automation Project 2020/src/test/java/com/usa/myrunner/MyRunner.java
@@ -5,8 +5,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
         @CucumberOptions(
 		features = { "Features" },
-		plugin = {"pretty", "html:target/cucumber-reports/cucumber.json",
-		"json:target/cucumber.json"},
+		plugin = {"pretty", "html:target/cucumber-reports/cucumber.json", "json:target/cucumber.json"},
 		dryRun = false,
 		monochrome  = true,
 		strict = true,
@@ -15,3 +14,4 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 public class MyRunner extends AbstractTestNGCucumberTests {
 
 }
+       
